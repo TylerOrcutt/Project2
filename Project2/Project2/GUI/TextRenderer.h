3980f3e2c8923fb585906c00d6799b8784f08200
TylerOrcutt <t.orcutt@eagle.clarion.edu>
2015-08-13 05:30:41 -0400

    TextRenderer.h reformat

--- Project2/Project2/GUI/TextRenderer.h
+++ Project2/Project2/GUI/TextRenderer.h
@@ -3,57 +3,47 @@
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
-
 #include <string>
 #include <sstream>
 #include <iostream>
 #include <pango/pangocairo.h>
-
 #include <string>
 #include <stdlib.h>
 
-
 class TextRenderer{
-private: GLuint texture;
+private:
+	GLuint texture;
 	int width, height;
-        float x=0,y=0;
-				float objectWidth=32,objectHeight=32;
+	float x=0,y=0;
+	float objectWidth=32,objectHeight=32;
 	std::string text;
 	int fontSize=16;
 	int padding=20;
-
-std::string font="arial";
-   public:
+	std::string font="arial";
+public:
 
 	TextRenderer(float posx,float posy,std::string text_str){
 		text=text_str;
 		x=posx;
 		y=posy;
 		initText();
-
 	}
 
 	void initText(){
-
-
-std::string temp_text=text;
-
-
+		std::string temp_text=text;
 		cairo_t *render_context;
 		cairo_surface_t *temp_surface;
 		cairo_surface_t *surface;
-     		unsigned char * surface_data = NULL;
+		unsigned char * surface_data = NULL;
 		PangoFontDescription *desc;
 
 		cairo_t *layout_context = create_layout_context();
 		PangoLayout *layout=pango_cairo_create_layout (layout_context);
-//	std::cout<<objectHeight<<"\n";
-if(objectHeight>32.f){
-//	std::cout<<"setting word wrap\n";
-pango_layout_set_width(layout,objectWidth*PANGO_SCALE);
-pango_layout_set_wrap(layout,PANGO_WRAP_WORD);
-}
 
+		if(objectHeight>32.f){
+			pango_layout_set_width(layout,objectWidth*PANGO_SCALE);
+			pango_layout_set_wrap(layout,PANGO_WRAP_WORD);
+		}
 		pango_layout_set_text(layout,temp_text.c_str(),-1);
 
 		std::stringstream ssize;
@@ -63,114 +53,93 @@ pango_layout_set_wrap(layout,PANGO_WRAP_WORD);
 		pango_layout_set_font_description(layout,desc);
 		pango_font_description_free(desc);
 
-
 		//get size
-
 		pango_layout_get_size(layout,&width,&height);
 		width/=PANGO_SCALE;
 		height/=PANGO_SCALE;
-     //width=200;
-		//create context
-		 surface_data=(unsigned char*)calloc(4*width*height,sizeof(unsigned char));
-		surface =cairo_image_surface_create_for_data(surface_data, CAIRO_FORMAT_ARGB32,width,height,4 * width);
 
+		//create context
+		surface_data=(unsigned char*)calloc(4*width*height,sizeof(unsigned char));
+		surface =cairo_image_surface_create_for_data(surface_data, CAIRO_FORMAT_ARGB32,width,height,4 * width);
 		render_context=cairo_create(surface);
 
-		 cairo_set_source_rgba(render_context,1.0f,1.0f,1.0f,1.0f);
+		cairo_set_source_rgba(render_context,1.0f,1.0f,1.0f,1.0f);
+		pango_cairo_update_layout(render_context, layout);
+		pango_cairo_show_layout(render_context, layout);
 
+		unsigned char *data=cairo_image_surface_get_data (surface);
+		create_texture(data);
+		//cairo_surface_write_to_png (surface, "Test.png");
 
-pango_cairo_update_layout(render_context, layout);
-	pango_cairo_show_layout(render_context, layout);
-
-	unsigned char *data=cairo_image_surface_get_data (surface);
-		 create_texture(data);
-//cairo_surface_write_to_png (surface, "Test.png");
-
-
-    free (surface_data);
-    g_object_unref (layout);
-    cairo_destroy (layout_context);
-    cairo_destroy (render_context);
-    cairo_surface_destroy (surface);
+		free (surface_data);
+		g_object_unref (layout);
+		cairo_destroy (layout_context);
+		cairo_destroy (render_context);
+		cairo_surface_destroy (surface);
 	}
-
-
 
 	cairo_t * create_layout_context(){
 		cairo_surface_t *temp;
 		cairo_t * context;
-
 		temp = cairo_image_surface_create (CAIRO_FORMAT_ARGB32, 0, 0);
 		context= cairo_create(temp);
 		cairo_surface_destroy(temp);
-	return context;
+		return context;
 	}
 
-
-
-   void create_texture(unsigned char *pixels){
-	//glTexImage2D(GL_TEXTURE_2D, 0, 4, width,height, 0,GL_BGRA, GL_UNSIGNED_BYTE, pixels);
-
-	glGenTextures(1,&texture);
-	glBindTexture(GL_TEXTURE_2D,texture);
-       glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexImage2D (GL_TEXTURE_2D,0,GL_RGBA,width,height,0,GL_BGRA,GL_UNSIGNED_BYTE, pixels);
-
-
-}
-
-
+	void create_texture(unsigned char *pixels){
+		glGenTextures(1,&texture);
+		glBindTexture(GL_TEXTURE_2D,texture);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexImage2D (GL_TEXTURE_2D,0,GL_RGBA,width,height,0,GL_BGRA,GL_UNSIGNED_BYTE, pixels);
+	}
 
 	void Draw(){
-		 glBindTexture (GL_TEXTURE_2D, texture);
-                     glColor3f (1.f,1.f,1.f);
+		glBindTexture (GL_TEXTURE_2D, texture);
+		glColor3f (1.f,1.f,1.f);
 
-                      glBegin (GL_QUADS);
-											float w=0,h=0;
-											float maxW=1.f,maxH=1.f;
-										float	maxWidth=width;
-										float maxHeight=height;
-											if(objectWidth>32 && objectHeight==32.f){
-												 if(width>(objectWidth-padding)){
-													 maxWidth=(objectWidth-padding);
-												w=1.f-((1.0f/width)*(objectWidth-padding));
-											}
-											}
+		float w=0,h=0;
+		float maxW=1.f,maxH=1.f;
+		float	maxWidth=width;
+		float maxHeight=height;
 
-											if(objectHeight>32){
-												if(width>objectWidth){
-													maxWidth=objectWidth;
-										//			maxHeight=objectHeight;
-												maxW =((1.f/width)*objectWidth);
-											}
-											//	std::cout<<maxW<<std::endl;
-												if(height>(objectHeight-(padding))){
-													maxHeight=(objectHeight-(padding));
-													h=1.f-((1.0f/height)*(objectHeight-(padding)));
-												}
-											}
-                       glTexCoord2f (w, h);
-                       glVertex2f (x, y+5);
+		if(objectWidth>32 && objectHeight==32.f){
+			if(width>(objectWidth-padding)){
+				maxWidth=(objectWidth-padding);
+				w=1.f-((1.0f/width)*(objectWidth-padding));
+			}
+		}
 
-                       glTexCoord2f (maxW, h);
-                       glVertex2f (x+maxWidth, y+5);
+		if(objectHeight>32){
+			if(width>objectWidth){
+				maxWidth=objectWidth;
+				maxW =((1.f/width)*objectWidth);
+			}
+			if(height>(objectHeight-(padding))){
+				maxHeight=(objectHeight-(padding));
+				h=1.f-((1.0f/height)*(objectHeight-(padding)));
+			}
+		}
 
-                       glTexCoord2f (maxW, maxH);
-                       glVertex2f (x+maxWidth , y+maxHeight+5);
+		glBegin (GL_QUADS);
+		glTexCoord2f (w, h);
+		glVertex2f (x, y+5);
+		glTexCoord2f (maxW, h);
+		glVertex2f (x+maxWidth, y+5);
+		glTexCoord2f (maxW, maxH);
+		glVertex2f (x+maxWidth , y+maxHeight+5);
+		glTexCoord2f (w, maxH);
+		glVertex2f (x, y+maxHeight+5);
+		glEnd ();
+	}
 
-                       glTexCoord2f (w, maxH);
-                       glVertex2f (x, y+maxHeight+5);
-                       glEnd ();
-
-	            }
-
-
-//getters/setters
+	//getters/setters
 	void setText(std::string _text){
 		text=_text;
 		initText();
 	}
+
 	std::string getText(){
 		return text;
 	}
@@ -178,6 +147,7 @@ pango_cairo_update_layout(render_context, layout);
 	int getWidth(){
 		return width;
 	}
+
 	int getHeight(){
 		return height;
 	}
@@ -185,33 +155,39 @@ pango_cairo_update_layout(render_context, layout);
 	void setX(float _x){
 		x=_x;
 	}
+
 	float getX(){
 		return x;
 	}
+
 	void setY(float _y){
 		y=_y;
 	}
+
 	float getY(){
 		return y;
 	}
-void setFont(std::string _font){
-	font=_font;
-	initText();
-}
-void setFontSize(int _fontSize){
-	fontSize = _fontSize;
-	initText();
-}
-void setObjectWidth(float _width){
-	objectWidth=_width;
-}void setObjectHeight(float _height){
-	objectHeight=_height;
-}
 
-void setPadding(float _padding){
-	padding=_padding;
-}
+	void setFont(std::string _font){
+		font=_font;
+		initText();
+	}
+
+	void setFontSize(int _fontSize){
+		fontSize = _fontSize;
+		initText();
+	}
+
+	void setObjectWidth(float _width){
+		objectWidth=_width;
+	}
+
+	void setObjectHeight(float _height){
+		objectHeight=_height;
+	}
+
+	void setPadding(float _padding){
+		padding=_padding;
+	}
 };
-
-
 #endif
